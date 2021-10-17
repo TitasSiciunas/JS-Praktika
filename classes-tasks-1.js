9a2dee48bb7188bf6e8c068896939a570ef433ab
Titas Sičiūnas <titas.siciunas@gmail.com>
2021-10-17 19:02:33 +0300

    Classes-tasks-1 completed

--- classes-tasks-1.js
+++ classes-tasks-1.js
@@ -37,3 +37,63 @@
 6. Po padidinimo, vėl atsispausdinkite objektus ir pažiūrėkite pakitusias Automobilis objektų savybes.
 
 */
+
+class Automobilis {
+  constructor(brand, model, price) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+}
+
+const car1 = new Automobilis('Mazda', '3', 3000);
+const car2 = new Automobilis('Tesla', 'Model 3', 50000);
+const car3 = new Automobilis('Subaru', 'WRX STI', 40000);
+
+console.group('Automobiliai:');
+{
+  console.log({
+    car1,
+    car2,
+    car3
+  });
+}
+console.groupEnd();
+
+console.group('Modeliai:');
+{
+  console.log(car1.model);
+  console.log(car2.model);
+  console.log(car3.model);
+}
+console.groupEnd();
+
+console.group('Markės:');
+{
+  console.log(car1.brand);
+  console.log(car2.brand);
+  console.log(car3.brand);
+}
+console.groupEnd();
+
+console.group('Kainos:');
+{
+  console.log(car1.price);
+  console.log(car2.price);
+  console.log(car3.price);
+}
+console.groupEnd();
+
+car1.price = car1.price + 100;
+car2.price = car2.price + 100;
+car3.price += 100;
+
+console.group('Automobiliai +100:');
+{
+  console.log({
+    car1,
+    car2,
+    car3
+  });
+}
+console.groupEnd();
